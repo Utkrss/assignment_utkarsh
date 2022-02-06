a943676d49c6b0c667e22ad4a50827a0232c48ce
Utkrss <86427747+Utkrss@users.noreply.github.com>
2022-02-06 14:12:52 +0530

    Delete Jan 31 to Feb 4 directory

--- Jan 31 to Feb 4/Condition2.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Condition2 {
-    public static void main(String[] args) {
-        int a = 55;
-        int b = 70;
-        System.out.println(a < 50 || a < b);
-    }
-}
