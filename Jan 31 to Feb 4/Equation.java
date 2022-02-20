5435121197968cb2d22398b9132a001951fdca38
UTKARSH CHAUDHARY <86427747+Utkrss@users.noreply.github.com>
2022-02-20 20:15:12 +0530

    Delete Jan 31 to Feb 4 directory

--- Jan 31 to Feb 4/Equation.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Equation {
-    public static void main(String[] args) {
-        double n = ((((8 + 2345) / 3) % 5) * 5);
-        System.out.println(n);
-
-    }
-}
